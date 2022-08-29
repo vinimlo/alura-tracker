470a68868adc8e87c87b20af2e740a88b4771b32
Vinícius Melo <vinimelo@riseup.net>
2022-08-29 16:33:31 -0300

    Adiciona view para tarefas e carrega na div correspondente

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,7 @@
       <Sidebar @themeChanged="toggleTheme" />
     </div>
     <div class="column is-three-quarter content">
-      <Form @onSaveTask="saveTask"/>
-      <div class="list">
-        <Task v-for="(task, index) in taskItems" :key="index" :task="task"/>
-        <Box v-if="isListEmpty">
-          No tasks yet :|
-        </Box> 
-      </div>
+      <RouterView></RouterView>
     </div>
   </main>
 </template>
@@ -18,34 +12,18 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Sidebar from './components/Sidebar.vue';
-import Form from './components/Form.vue';
-import Task from './components/Task.vue';
-import Box from './components/Box.vue';
-import ITask from './interfaces/ITask';
 
 export default defineComponent({
   name: "App",
   components: {
     Sidebar,
-    Form,
-    Task,
-    Box
   },
   data() {
     return {
-      taskItems: [] as ITask[],
       isDarkModeOn: false
     }
   },
-  computed: {
-    isListEmpty(): boolean {
-      return this.taskItems.length === 0;
-    }
-  },
   methods: {
-    saveTask(task: ITask) {
-      this.taskItems.push(task);
-    },
     toggleTheme(isDarkModeOn: boolean) {
       this.isDarkModeOn = isDarkModeOn;
     }
@@ -54,17 +32,16 @@ export default defineComponent({
 </script>
 
 <style>
-.list {
-  padding: 1.25rem;
-}
 main {
   --bg-primary: #FFFFFF;
   --text-primary: #000000;
 }
+
 main.dark-mode {
   --bg-primary: #2b2d42;
   --text-primary: #DDDDDD;
 }
+
 .content {
   background-color: var(--bg-primary);
 }
---
+++ src/views/Tasks.vue
@@ -0,0 +1,47 @@
+<template>
+  <Form @onSaveTask="saveTask" />
+  <div class="list">
+    <Task v-for="(task, index) in taskItems" :key="index" :task="task" />
+    <Box v-if="isListEmpty">
+      No tasks yet :|
+    </Box>
+  </div>
+</template>
+  
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Form from '../components/Form.vue';
+import Task from '../components/Task.vue';
+import Box from '../components/Box.vue';
+import ITask from '../interfaces/ITask';
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Form,
+    Task,
+    Box
+  },
+  data() {
+    return {
+      taskItems: [] as ITask[]
+    }
+  },
+  computed: {
+    isListEmpty(): boolean {
+      return this.taskItems.length === 0;
+    }
+  },
+  methods: {
+    saveTask(task: ITask) {
+      this.taskItems.push(task);
+    }
+  }
+});
+</script>
+  
+<style>
+.list {
+  padding: 1.25rem;
+}
+</style>
